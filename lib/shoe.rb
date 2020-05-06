72d319a901c4d00fafbef280b25e626af4db32d3
github username <github email address>
2020-05-06 07:35:11 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,17 +9,13 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-
-  def uniq_brands
-    BRANDS.uniq.size
   end
 
 end
